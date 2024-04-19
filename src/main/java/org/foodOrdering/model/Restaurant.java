b1e93e28976cfb2cc5542f0d4f09529cb1553cd0
tonnymuchui <tonykanyingah@gmail.com>
2024-04-20 02:40:17 +0300

    Add Restaurant Model

---
+++ src/main/java/org/foodOrdering/model/Restaurant.java
@@ -0,0 +1,82 @@
+package org.foodOrdering.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @OneToOne
+    private User owner;
+    
+    private String name;
+    private String description;
+    private String cuisineType;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+    
+    @Embedded
+    private ContactInformation contactInformation;
+    
+    private String openingHours;
+    
+    @JsonIgnore
+    @OneToMany(mappedBy = "restaurant",cascade = CascadeType.ALL,orphanRemoval = true)
+    @ToString.Exclude
+    private List<Review>reviews=new ArrayList<>();
+    
+    @JsonIgnore
+    @OneToMany(mappedBy="restaurant",cascade=CascadeType.ALL,orphanRemoval = true)
+    @ToString.Exclude
+    private List<Order> orders=new ArrayList<>();
+    
+    private int numRating;
+
+    @ElementCollection
+    @Column(length = 1000)
+    private List<String> images;
+   
+    private LocalDateTime registrationDate;
+    
+    private boolean open;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "restaurant",cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<Food> foods=new ArrayList<>();
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Restaurant that = (Restaurant) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
+
