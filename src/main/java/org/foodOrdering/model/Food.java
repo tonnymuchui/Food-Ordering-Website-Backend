56a1ea9059bec7f111e65ce345a0b17520865951
tonnymuchui <tonykanyingah@gmail.com>
2024-04-20 02:38:08 +0300

    Add Food Model

---
+++ src/main/java/org/foodOrdering/model/Food.java
@@ -0,0 +1,68 @@
+package org.foodOrdering.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String description;
+    private Long price;
+    
+    @ManyToOne
+    private Category foodCategory;
+
+
+    @ElementCollection
+    @Column(length = 1000)
+    private List<String> images;
+
+    private boolean available;
+
+//    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+    
+    private boolean isVegetarian;
+    private boolean isSeasonal;
+    
+    @ManyToMany
+    @ToString.Exclude
+    private List<IngredientsItem> ingredients=new ArrayList<>();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Food food = (Food) o;
+        return getId() != null && Objects.equals(getId(), food.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
