0dd58539b939104a0eb9ee8e934edb9dfd7d88ea
tonnymuchui <tonykanyingah@gmail.com>
2024-04-20 02:30:45 +0300

    Add User Model

---
+++ src/main/java/org/foodOrdering/model/User.java
@@ -0,0 +1,61 @@
+package org.foodOrdering.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.foodOrdering.domain.USER_ROLE;
+import org.foodOrdering.dto.RestaurantDto;
+import org.hibernate.proxy.HibernateProxy;
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String fullName;
+	private String email;
+	private String password;
+
+	private USER_ROLE role;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+	@ToString.Exclude
+	private List<Order> orders;
+
+	@ElementCollection
+	private List<RestaurantDto> favorites=new ArrayList<>();
+	
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	@ToString.Exclude
+	private List<RabbitConnectionDetails.Address> addresses = new ArrayList<>();
+	
+	private String status;
+
+	@Override
+	public final boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null) return false;
+		Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+		Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+		if (thisEffectiveClass != oEffectiveClass) return false;
+		User user = (User) o;
+		return getId() != null && Objects.equals(getId(), user.getId());
+	}
+
+	@Override
+	public final int hashCode() {
+		return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+	}
+}
