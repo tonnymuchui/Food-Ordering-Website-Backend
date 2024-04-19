0aeec47e47218a333e4849276e9fe431fcb7da2c
tonnymuchui <tonykanyingah@gmail.com>
2024-04-20 02:37:59 +0300

    Add Event Model

---
+++ src/main/java/org/foodOrdering/model/Events.java
@@ -0,0 +1,50 @@
+package org.foodOrdering.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+public class Events {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String image;
+
+    private String startedAt;
+
+    private String endsAt;
+
+    private String name;
+
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private String location;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Events events = (Events) o;
+        return getId() != null && Objects.equals(getId(), events.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
