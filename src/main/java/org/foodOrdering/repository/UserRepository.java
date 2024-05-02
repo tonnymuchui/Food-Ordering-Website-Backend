d9fac9f1dbc4b17a513ec4f13e55ec4d059249d8
tonnymuchui <tonykanyingah@gmail.com>
2024-05-02 12:23:12 +0300

    Add UserRepository

---
+++ src/main/java/org/foodOrdering/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package org.foodOrdering.repository;
+
+import org.foodOrdering.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+	
+	@Query("SELECT u FROM User u Where u.status='PENDING'")
+	public List<User> getPenddingRestaurantOwners();
+	
+	public User findByEmail(String username);
+
+}
