fb342c6a4688ec5f8a4f8cc2e2f49308492538ec
tonnymuchui <tonykanyingah@gmail.com>
2024-05-02 12:24:01 +0300

    Add RestaurantRepository

---
+++ src/main/java/org/foodOrdering/repository/RestaurantRepository.java
@@ -0,0 +1,18 @@
+
+package org.foodOrdering.repository;
+
+import org.foodOrdering.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+	@Query("SELECT r FROM Restaurant r WHERE lower(r.name) LIKE lower(concat('%', :query, '%')) OR lower(r.cuisineType) LIKE lower(concat('%', :query, '%'))")
+	List<Restaurant> findBySearchQuery(String query);
+
+	Restaurant findByOwnerId(Long userId);
+
+
+
+}
