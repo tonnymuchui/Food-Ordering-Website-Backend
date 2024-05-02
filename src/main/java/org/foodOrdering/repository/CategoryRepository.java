f2a642e28ff4654e99ed0eb90657ad40457539bc
tonnymuchui <tonykanyingah@gmail.com>
2024-05-02 12:27:47 +0300

    Add CategoryRepository

---
+++ src/main/java/org/foodOrdering/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package org.foodOrdering.repository;
+
+import org.foodOrdering.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+	public List<Category> findByRestaurantId(Long id);
+}
