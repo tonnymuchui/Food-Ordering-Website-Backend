6f889a96f5d1e43d2216fbfaa92fa668b9a4fe86
tonnymuchui <tonykanyingah@gmail.com>
2024-05-07 13:50:51 +0300

    Add CategoryController

---
+++ src/main/java/org/foodOrdering/controller/CategoryController.java
@@ -0,0 +1,48 @@
+package org.foodOrdering.controller;
+
+import org.foodOrdering.Exception.RestaurantException;
+import org.foodOrdering.Exception.UserException;
+import org.foodOrdering.model.Category;
+import org.foodOrdering.model.User;
+import org.foodOrdering.service.CategoryService;
+import org.foodOrdering.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+
+	private final CategoryService categoryService;
+	private final UserService userService;
+
+	@Autowired
+	public CategoryController(CategoryService categoryService, UserService userService) {
+		this.categoryService = categoryService;
+		this.userService = userService;
+	}
+	
+	@PostMapping("/admin/category")
+	public ResponseEntity<Category> createdCategory(
+			@RequestHeader("Authorization")String jwt,
+			@RequestBody Category category) throws RestaurantException, UserException {
+		User user=userService.findUserProfileByJwt(jwt);
+		
+		Category createdCategory=categoryService.createCategory(category.getName(), user.getId());
+		return new ResponseEntity<Category>(createdCategory, HttpStatus.OK);
+	}
+	
+	@GetMapping("/category/restaurant/{id}")
+	public ResponseEntity<List<Category>> getRestaurantsCategory(
+			@PathVariable Long id,
+			@RequestHeader("Authorization")String jwt) throws RestaurantException, UserException {
+		User user=userService.findUserProfileByJwt(jwt);
+		List<Category> categories=categoryService.findCategoryByRestaurantId(id);
+		return new ResponseEntity<>(categories,HttpStatus.OK);
+	}
+
+}
