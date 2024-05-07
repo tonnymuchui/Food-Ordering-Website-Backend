f9244045dd0ff263d845dc8b4cba8049dfdf46ab
tonnymuchui <tonykanyingah@gmail.com>
2024-05-07 13:40:48 +0300

    Add ReviewException

---
+++ src/main/java/org/foodOrdering/Exception/ReviewException.java
@@ -0,0 +1,11 @@
+package org.foodOrdering.Exception;
+
+public class ReviewException extends Exception {
+
+	public ReviewException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
