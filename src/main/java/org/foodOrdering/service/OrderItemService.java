d1c3b1b8d5bd43edcafba19fe1354acf9b8afc13
tonnymuchui <tonykanyingah@gmail.com>
2024-05-07 13:33:58 +0300

    Add OrderService

---
+++ src/main/java/org/foodOrdering/service/OrderItemService.java
@@ -0,0 +1,9 @@
+package org.foodOrdering.service;
+
+import org.foodOrdering.model.OrderItem;
+
+public interface OrderItemService {
+	
+	public OrderItem createOrderItem(OrderItem orderItem);
+
+}
---
+++ src/main/java/org/foodOrdering/service/OrderItemServiceImplementation.java
@@ -0,0 +1,34 @@
+package org.foodOrdering.service;
+
+import org.foodOrdering.model.OrderItem;
+import org.foodOrdering.repository.OrderItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderItemServiceImplementation implements OrderItemService {
+	@Autowired
+	 private OrderItemRepository orderItemRepository;
+
+	    @Override
+	    public OrderItem createOrderItem(OrderItem orderItem) {
+	    	
+	    	OrderItem newOrderItem=new OrderItem();
+//	    	newOrderItem.setMenuItem(orderItem.getMenuItem());
+//	    	newOrderItem.setOrder(orderItem.getOrder());
+	    	newOrderItem.setQuantity(orderItem.getQuantity());
+	        return orderItemRepository.save(newOrderItem);
+	    }
+	    
+
+
+
+
+
+		
+
+
+
+
+
+}
