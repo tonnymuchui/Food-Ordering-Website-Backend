856d583cd91b15f478a30fe92b7c1747792e6c2b
tonnymuchui <tonykanyingah@gmail.com>
2024-05-07 12:06:28 +0300

    Add User

---
+++ src/main/java/org/foodOrdering/service/UserService.java
@@ -0,0 +1,22 @@
+package org.foodOrdering.service;
+
+import org.foodOrdering.Exception.UserException;
+import org.foodOrdering.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+	public User findUserProfileByJwt(String jwt) throws UserException;
+	
+	public User findUserByEmail(String email) throws UserException;
+
+	public List<User> findAllUsers();
+
+	public List<User> getPenddingRestaurantOwner();
+
+	void updatePassword(User user, String newPassword);
+
+	void sendPasswordResetEmail(User user);
+
+}
