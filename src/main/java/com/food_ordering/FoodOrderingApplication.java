178d055505b8c7006b49a66db23e17e12e9babea
tonnymuchui <tonykanyingah@gmail.com>
2024-06-14 01:24:50 +0300

    Add dependecies

---
+++ src/main/java/com/food_ordering/FoodOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.food_ordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/food_ordering/FoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
